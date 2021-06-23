c03c02fa2507bfb5c4b43a44b1df1062b6d869c1
weikx <weikexin666@gmail.com>
2021-06-23 23:02:25 +0800

    update

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# 当温度低于37.5°时，继电器为低电平
 import RPi.GPIO as GPIO
 import Adafruit_DHT
 import time
@@ -11,17 +10,17 @@ GPIO.setup(relayPort, GPIO.OUT)
 
 
 def getTemp():
-  while True:
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT22Port)
-    heating(temperature)
-    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-    time.sleep(2)
+    while True:
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT22Port)
+        heating(temperature)
+        print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+        time.sleep(2)
 
 
 def heating(temp):
-  if (temp >= 37.5):
-    GPIO.output(relayPort, GPIO.HIGH)
-    print(f'停止加热{temp}')
-  else:
-    GPIO.output(relayPort, GPIO.LOW)
-    print(f'正在{temp}')
+    if (temp >= 37.5):
+        GPIO.output(relayPort, GPIO.HIGH)
+        print(f'停止加热{temp}')
+    else:
+        GPIO.output(relayPort, GPIO.LOW)
+        print(f'正在{temp}')
